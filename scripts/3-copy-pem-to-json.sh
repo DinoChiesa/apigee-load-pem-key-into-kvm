2cf52f51daec9cc66c45518bf2aca6d992b6e4e1
DinoChiesa <dchiesa@google.com>
2025-06-25 17:50:20 -0700

    chore: Add comments for KVM name validation

--- scripts/3-copy-pem-to-json.sh
+++ scripts/3-copy-pem-to-json.sh
@@ -11,3 +11,12 @@ check_required_commands openssl jq
 # We use jq to parse the JSON and mapfile to read the names into a shell
 # array named 'kvm_names'.
 mapfile -t kvm_names < <(apigeecli kvms list --env "${APIGEE_ENV}" --org "${APIGEE_PROJECT}" | jq -r '.[]')
+
+# AI! Prompt request input from the user:
+#
+#   Name of the to-be-created environment-scoped Key Value Map:?
+#
+# Store this value into a local variable here.
+# Then, check to see if the Name the user specified is in the list of
+# names retrieved by the prior command.  If present, print
+# a message like "that KVM already exists. Exiting. " and exit. 
